3f0bdedc003106f47501e39e0e7ded421ff5a8ed
Lucie Santer <69685489+Luciolesnt@users.noreply.github.com>
2023-02-28 14:27:15 +0100

    Feat(Test ci cd): modify test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders DockerHub Image link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/DockerHub Image/i);
   expect(linkElement).toBeInTheDocument();
 });
